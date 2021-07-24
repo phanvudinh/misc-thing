81eca35b2ed3780d7b120b7f08e40b2da645814c
Dinh Phan <itphanvudinh@gmail.com>
2021-07-24 21:46:59 +0700

    modify run message

--- run/run.go
+++ run/run.go
@@ -3,5 +3,5 @@ package run
 import "fmt"
 
 func Run(howfar int) string {
-	return fmt.Sprintf("I'm running too far. It is about %dkm", howfar)
+	return fmt.Sprintf("I'm running too far. It is about %dkm. That is amazing!!!git", howfar)
 }
